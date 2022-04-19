691ee1987b9de719769f8b684e1cab37150ca9ab
JosephBrooke <joseph.brooke1@gmail.com>
2022-04-19 15:28:46 +0100

    added flask app

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-'''
+"""
 STEP1: load the initial data set 
 which consists of three CSV files 
 and translate it into JSON format
-'''
+"""
 import pandas as pd
+
 airport_df = pd.read_csv("./data/airports.csv")
 airport_frequencies_df = pd.read_csv("./data/airport-frequencies.csv")
 runways_df = pd.read_csv("./data/runways.csv")
 
 # export dataframes to json
 from os import path, mkdir
-if not path.isdir('json_data'):
-    mkdir('json_data')
-airport_df.to_json(r'./json_data/airport.json')
+
+if not path.isdir("json_data"):
+    mkdir("json_data")
+airport_df.to_json(r"./json_data/airport.json")
 
 # connect to sqllite, a built in python module
 import sqlite3
-#create db connection
-conn = sqlite3.connect('data.db')
+
+# create db connection
+conn = sqlite3.connect("data.db")
 # execute some sql command
 c = conn.cursor()
+
+# plot some data
+import matplotlib.pyplot as plt
+
+plt.plot(airport_df["type"], airport_df["name"])
+plt.show()
+
+print(airport_df.to_html())
