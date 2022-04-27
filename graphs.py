a027d6bcef9c07d709e46fb3e77ad223db6e7d68
JosephBrooke <joseph.brooke1@gmail.com>
2022-04-28 00:12:59 +0100

    initial draw small airport frequency

--- graphs.py
+++ graphs.py
@@ -4,28 +4,55 @@ import matplotlib.pyplot as plt
 import io
 import db
 
-figure = plt.figure()
-axes = figure.add_subplot(1, 1, 1)
-axes.bar([1, 2, 3, 4], [3, 4, 5, 25], tick_label=["a", "b", "c", "d"])
-
 
 def draw_airport():
-    metadata = db.MetaData(bind=None)
-    airport = db.Table("airports", metadata, autoload=True, autoload_with=db.engine)
-    select_data = db.select(airport)
-
     sql = """
         SELECT frequency_mhz, airport_ref FROM db.`airport-frequencies`
         JOIN db.airports ON db.airports.id=db.`airport-frequencies`.airport_ref
         AND db.airports.type='small_airport';
         """
+    sql_text = db.text(sql)
     # all datas from the database
-    rp = db.connection.execute(select_data)
-    result = rp.fetchall()
+    rp = db.connection.execute(sql_text)
+    results = rp.fetchall()
 
-    # get the result as list
-    print(type(result[10]))
+    # create a bin size that reflects the width
+    # get the max frequency value from DB
+    sql_max = """
+             SELECT MAX(frequency_mhz), airport_ref, db.airports.name FROM db.`airport-frequencies`
+            JOIN db.airports ON db.airports.id=db.`airport-frequencies`.airport_ref
+            AND db.airports.type='small_airport';
+            """
+    max =db.connection.execute(db.text(sql_max))
+    max_val = max.fetchall()[0][0]
+
+    # get the min frequency value from db
+    sql_min = """
+            SELECT MIN(frequency_mhz), airport_ref, db.airports.name FROM db.`airport-frequencies`
+            JOIN db.airports ON db.airports.id=db.`airport-frequencies`.airport_ref
+            AND db.airports.type='small_airport';
+            """
+    max =db.connection.execute(db.text(sql_min))
+    min_val = max.fetchall()[0][0]
+
+    # total width and allocate binsize
+    bin_width = 15 # create a set number of bins
+    total_width = max_val - min_val
+    print('total_width:', total_width)
+    bins = int(total_width) // bin_width
+
+    # average freqluency
+    sql_avg = """
+        SELECT AVG(frequency_mhz), airport_ref, db.airports.name FROM db.`airport-frequencies`
+        JOIN db.airports ON db.airports.id=db.`airport-frequencies`.airport_ref
+        AND db.airports.type='small_airport';"""
+    avg =db.connection.execute(db.text(sql_avg))
+    avg_val = avg.fetchall()[0][0]
+
+    plt.hist(x=[result[0] for result in results], bins=bins, edgecolor="yellow", color="green")
+    plt.xlim(min_val, avg_val + bin_width*5)
+    plt.title("communication frequencies used by small_airports")
+    plt.show()
 
 
 draw_airport()
-plt.show()
